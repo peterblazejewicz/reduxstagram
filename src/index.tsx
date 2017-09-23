5638fb35223577c15126ca4966a21ffc1c6e7172
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2017-09-23 10:09:47 +0200

    Stylus build support

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import './styles/style.css';
 
 ReactDOM.render(
   <p>Hello!</p>,
