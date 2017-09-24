b59de25c4abfb1165447f2e6f7e3776f4c2abfb7
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2017-09-24 21:03:38 +0200

    React transition groups

--- src/components/Photo.tsx
+++ src/components/Photo.tsx
@@ -1,17 +1,33 @@
 import React, { SFC } from 'react';
 import { Link } from 'react-router-dom';
 import { Post } from '../types/index';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+
 interface PhotoProps {
   index: number;
   post: Post;
 }
+
 const Photo: SFC<PhotoProps> = ({ post, index }) => (
   <figure className="grid-figure">
     <div className="grid-photo-wrap">
       <Link to={`/view/${post.code}`}>
         <img src={post.display_src} alt={post.caption} className="grid-photo" />
       </Link>
+      <TransitionGroup>
+        <CSSTransition classNames="like" timeout={{ enter: 500, exit: 500 }}>
+          <span className="likes-heart" key={post.likes}>
+            {post.likes}
+          </span>
+        </CSSTransition>
+      </TransitionGroup>
     </div>
+    <figcaption>
+      <p>{post.caption}</p>
+      <div className="control-buttons">
+        <button className="likes">&hearts; {post.likes}</button>
+      </div>
+    </figcaption>
   </figure>
 );
 export default Photo;
