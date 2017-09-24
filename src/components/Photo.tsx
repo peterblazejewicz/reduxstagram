961f9a4839a26eaf1275b8d0af434353e5ac35fd
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2017-09-24 21:54:52 +0200

    Split Photo internally into Figure/FigureCaption

--- src/components/Photo.tsx
+++ src/components/Photo.tsx
@@ -1,33 +1,61 @@
 import React, { SFC } from 'react';
 import { Link } from 'react-router-dom';
-import { Post } from '../types/index';
+import { Post, Comments } from '../types/index';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import { IncrementLikes } from '../actions';
 
 interface PhotoProps {
   index: number;
   post: Post;
+  comments: Comments;
+  increment: (index: number) => IncrementLikes;
 }
 
-const Photo: SFC<PhotoProps> = ({ post, index }) => (
+const Caption: SFC<PhotoProps> = props => (
+  <figcaption>
+    <p>{props.post.caption}</p>
+    <div className="control-buttons">
+      <button
+        onClick={props.increment.bind(null, props.index)}
+        className="likes"
+      >
+        &hearts; {props.post.likes}
+      </button>
+      <Link className="button" to={`/view/${props.post.code}`}>
+        <span className="comment-count">
+          <span className="speech-bubble" />
+          {props.comments[props.post.code] ? (
+            props.comments[props.post.code].length
+          ) : (
+            0
+          )}
+        </span>
+      </Link>
+    </div>
+  </figcaption>
+);
+
+const Figure: SFC<PhotoProps> = props => (
   <figure className="grid-figure">
     <div className="grid-photo-wrap">
-      <Link to={`/view/${post.code}`}>
-        <img src={post.display_src} alt={post.caption} className="grid-photo" />
+      <Link to={`/view/${props.post.code}`}>
+        <img
+          src={props.post.display_src}
+          alt={props.post.caption}
+          className="grid-photo"
+        />
       </Link>
       <TransitionGroup>
         <CSSTransition classNames="like" timeout={{ enter: 500, exit: 500 }}>
-          <span className="likes-heart" key={post.likes}>
-            {post.likes}
+          <span className="likes-heart" key={props.post.likes}>
+            {props.post.likes}
           </span>
         </CSSTransition>
       </TransitionGroup>
     </div>
-    <figcaption>
-      <p>{post.caption}</p>
-      <div className="control-buttons">
-        <button className="likes">&hearts; {post.likes}</button>
-      </div>
-    </figcaption>
+    <Caption {...props} />
   </figure>
 );
+
+const Photo: SFC<PhotoProps> = props => <Figure {...props} />;
 export default Photo;
